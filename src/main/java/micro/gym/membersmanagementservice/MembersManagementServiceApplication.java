0a278bd2bd8f90e5678b56d03d8a512c499fecac
Pabl0FPP <pinedapablo6718@gmail.com>
2026-02-18 22:50:49 -0500

    feat: add base project

---
+++ src/main/java/micro/gym/membersmanagementservice/MembersManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package micro.gym.membersmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembersManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembersManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/micro/gym/membersmanagementservice/MembersManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.gym.membersmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
